f3be1ea922a232e7341f89e93a4b9c3e4eda8e2f
demo <vidhyashmi@gmail.com>
2025-12-16 08:23:58 -0800

    for loop added

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 console.log('Hello, World!');
 console.log('This is the index.js file.');
+for (let i = 1; i <= 5; i++) {
+    console.log(`Counting: ${i}`);
+}
+console.log('Welcome to the project!');
